d32d57519d20cd2aab5f4969b230d2f40b8cb290
Davide <ciampad06@gmail.com>
2020-11-16 16:06:59 +0100

    Fine esercizio con aggiunta del css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var listaCognomi=['Pesce', "Castenetto", "Passaro", "Grimaldi", "Rossi", "Verdi", "Bianchi"];
 console.log(listaCognomi);
-document.getElementById("ul").innerHTML=listaCognomi
+document.getElementById("ul").innerHTML=listaCognomi;
 
 var cognome= prompt("qual'e' il tuo cognome?");
 console.log(cognome);
@@ -13,10 +13,10 @@ listaCognomi.sort();
 console.log(listaCognomi);
 document.getElementById("ol").innerHTML=listaCognomi;
 
-for (var i = 1; i <= listaCognomi.length; i++) {
+for (var i = 0; i <= listaCognomi.length; i++) {
   if (listaCognomi[i] == cognome) {
     var posizione= i;
     console.log(i);
-    document.getElementById("number_order").innerHTML= i + 1;
+    document.getElementById("number_order").innerHTML= posizione + 1;
   }
 }
